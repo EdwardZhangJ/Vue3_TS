bb0ee157d16e39ce8b6ce68663d31ef8f20b8a00
EdwardWest <edward_job@163.com>
2022-06-13 10:54:59 +0800

    test

---
+++ js/test.js
@@ -0,0 +1,22 @@
+
+// timestamp4 = new Date(1654585142 * 1000)
+
+// console.log(timestamp4);
+// console.log(timestamp4.toLocaleDateString().replace(/\//g, "-") + " " + timestamp4.toTimeString().substr(0, 8))
+
+
+// timeStamp = new Date(1654596620 * 1000)
+
+// console.log(timeStamp);
+// console.log(timeStamp.toLocaleDateString().replace(/\//g, "-") + " " + timeStamp.toTimeString().substring(0, 8))
+
+
+var str = '{"result":{"resultCode":"000000","resultMessage":"成功!"},"data":{"fileSaveName":"applet/im/ufEp4XrnyYgf4181f50ed80905aed98ad2c8a339800a1535216359552581632.png","chunks":null,"fileUrl":"http://192.168.3.210:8380/file-service/file/view/applet/im/ufEp4XrnyYgf4181f50ed80905aed98ad2c8a339800a1535216359552581632.png"}})';
+
+// str.slice(0,str.length-1)
+
+// console.log(str)
+
+
+
+console.log(str)
