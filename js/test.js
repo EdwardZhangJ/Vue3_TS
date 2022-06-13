e1592933a735178ea273e4d2e7a8fb247a7cc793
EdwardWest <edward_job@163.com>
2022-06-13 17:24:20 +0800

    day06 webpack打包

--- js/test.js
+++ js/test.js
@@ -10,7 +10,6 @@
 // console.log(timeStamp);
 // console.log(timeStamp.toLocaleDateString().replace(/\//g, "-") + " " + timeStamp.toTimeString().substring(0, 8))
 
-
 var str = '{"result":{"resultCode":"000000","resultMessage":"成功!"},"data":{"fileSaveName":"applet/im/ufEp4XrnyYgf4181f50ed80905aed98ad2c8a339800a1535216359552581632.png","chunks":null,"fileUrl":"http://192.168.3.210:8380/file-service/file/view/applet/im/ufEp4XrnyYgf4181f50ed80905aed98ad2c8a339800a1535216359552581632.png"}})';
 
 // str.slice(0,str.length-1)
